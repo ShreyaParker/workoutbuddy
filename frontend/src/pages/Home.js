3873eaf5799294263790c80109d74bb060f159ac
Shreya <shreyaparker317@gmail.com>
2022-07-02 20:00:09 +0530

    Adding react context

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,31 +1,36 @@
-import {useEffect, useState} from "react";
+import { useEffect } from "react"
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
+
+// components
 import WorkoutDetails from "../components/WorkoutDetails"
-import WorkoutForm from "../components/WorkoutForm";
+import WorkoutForm from "../components/WorkoutForm"
+
 const Home = () => {
+    const { workouts, dispatch } = useWorkoutsContext()
 
-    const [workouts,setWorkouts] = useState(null);
-    useEffect(()=>{
+    useEffect(() => {
         const fetchWorkouts = async () => {
-            const response = await fetch("/api/workouts")
-            const json = await response.json();
+            const response = await fetch('/api/workouts')
+            const json = await response.json()
 
-            if(response.ok){
-                setWorkouts(json)
+            if (response.ok) {
+                dispatch({type: 'SET_WORKOUTS', payload: json})
             }
         }
-        fetchWorkouts();
-        },[]);
+
+        fetchWorkouts()
+    }, [dispatch])
 
     return (
         <div className="home">
             <div className="workouts">
-                {workouts && workouts.map((workout)=>(
-                    <WorkoutDetails key={workout._id} workout={workout}/>
+                {workouts && workouts.map(workout => (
+                    <WorkoutDetails workout={workout} key={workout._id} />
                 ))}
             </div>
-            <WorkoutForm/>
+            <WorkoutForm />
         </div>
     )
 }
 
-export default Home;
+export default Home
